f4fce894095667670435386456745e89c968f856
Kevin Mannix <kmannix@bu.edu>
2014-11-25 21:22:14 -0500

    Close to finishing. Need to QA

--- Lab7/Lab7/Form1.cs
+++ Lab7/Lab7/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace Lab7
 {
@@ -29,27 +30,28 @@ namespace Lab7
 
         private void encryptButton_Click(object sender, EventArgs e)
         {
-            if(textInputsValid()) {
-
+            if(textInputsValid() && checkPathExists()) {
+                string text = File.ReadAllText(filePathText);
+                int size = text.Length;
+                byte[] key = createByteArray();
+                byte[] encrypted = EncryptStringToBytes(text, key, key);
             }
         }
 
         private void decryptButton_Click(object sender, EventArgs e)
         {
-            if (textInputsValid()) {
+            if (textInputsValid() && checkPathExists() && checkForDESFileExtension() && checkForOverwriteFile()                     )
+            {
 
             }
         }
 
         private void browseButton_Click(object sender, EventArgs e)
         {
-            int size = -1;
             DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
             if (result == DialogResult.OK)  {
                 string file = openFileDialog1.FileName;
                 try {
-                    string text = File.ReadAllText(file);
-                    size = text.Length;
                     fileNameInput.Text = openFileDialog1.FileName;
                 }
                 catch (Exception) {
@@ -86,6 +88,111 @@ namespace Lab7
          * Helper Methods
          * 
          * * * * * * * * * * * * * * */
+
+        private byte[] createByteArray()
+        {
+            byte[] byteArr = new byte[8];
+            char[] key_temp = keyText.ToCharArray();
+            for (int i = 0; i < key_temp.Length; i++)
+            {
+                byte c = (byte) (Convert.ToUInt16(key_temp[0]) >> 8);
+                byteArr[i % 8] = (byte) (byteArr[i % 8] + c); 
+            }
+            return byteArr;
+        }
+
+        static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments. 
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+            byte[] encrypted;
+            // Create an TripleDESCryptoServiceProvider object 
+            // with the specified key and IV. 
+            using (TripleDESCryptoServiceProvider tdsAlg = new TripleDESCryptoServiceProvider())
+            {
+                tdsAlg.Key = Key;
+                tdsAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform encryptor = tdsAlg.CreateEncryptor(tdsAlg.Key, tdsAlg.IV);
+
+                // Create the streams used for encryption. 
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+
+            // Return the encrypted bytes from the memory stream. 
+            return encrypted;
+
+        }
+
+        static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments. 
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+
+            // Declare the string used to hold 
+            // the decrypted text. 
+            string plaintext = null;
+
+            // Create an TripleDESCryptoServiceProvider object 
+            // with the specified key and IV. 
+            using (TripleDESCryptoServiceProvider tdsAlg = new TripleDESCryptoServiceProvider())
+            {
+                tdsAlg.Key = Key;
+                tdsAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = tdsAlg.CreateDecryptor(tdsAlg.Key, tdsAlg.IV);
+
+                // Create the streams used for decryption. 
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream 
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+
+        }
+
+        private void saveEncryptedFile(byte[] bArr, String path)
+        {
+            String newFilePath = Path.ChangeExtension(path, Path.GetExtension(path) + ".des");
+            File.WriteAllBytes(newFilePath, bArr);
+        }
 
         /* * * * * * * * * * * * * * *
          * 
